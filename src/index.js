d3995eda0eab6f3cb8b83e2fee1692d6ad96946b
prakharDvedi <prakhardwivedi.12@gmail.com>
2025-12-27 21:38:30 +0530

    added docker file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Internal CI/CD System Node Service",
+    message: "CI/CD System Service",
     status: "running",
   });
 });
