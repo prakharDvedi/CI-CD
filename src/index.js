493da4c0e4fb711194341d534ec6e51591cb0b44
prakharDvedi <prakhardwivedi.12@gmail.com>
2025-12-27 21:26:06 +0530

    first node

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Internal CI/CD System Node Service",
+    status: "running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
